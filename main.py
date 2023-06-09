f9ea1646890b714e1f945d63e68f739b960ccaa3
SaMMYFrosT <sammy.frost.221b@gmail.com>
2023-06-09 22:19:00 +0530

    Added Police

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ screen = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption("Thief Game ")
 
+image_path = "./Police (1).png"
+
+image = pygame.image.load(image_path)
+
+image_x = 50
+image_y = 100
+
 ball = {
     1: (100, 100),
     2: (400, 100),
@@ -31,6 +38,18 @@ ball = {
     7: (100, 500),
     8: (400, 500),
     9: (700, 500),
+}
+
+ball_1 = {
+    1: (50, 50),
+    2: (350, 50),
+    3: (650, 50),
+    4: (50, 250),
+    5: (350, 250),
+    6: (650, 250),
+    7: (50, 450),
+    8: (350, 450),
+    9: (650, 450),
 }
 
 x, y = 100, 100
@@ -68,23 +87,25 @@ while True:
                 pygame.display.update()
 
     screen.fill((255, 0, 0))
-    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(100, 100), radius=50)
-    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(400, 100), radius=50)
-    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(700, 100), radius=50)
-    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(100, 300), radius=50)
-    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(400, 300), radius=50)
-    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(700, 300), radius=50)
-    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(100, 500), radius=50)
-    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(400, 500), radius=50)
-    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(700, 500), radius=50)
+    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(100, 100), radius=10)
+    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(400, 100), radius=10)
+    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(700, 100), radius=10)
+    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(100, 300), radius=10)
+    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(400, 300), radius=10)
+    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(700, 300), radius=10)
+    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(100, 500), radius=10)
+    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(400, 500), radius=10)
+    pygame.draw.circle(surface=screen, color=(0, 0, 0), center=(700, 500), radius=10)
 
     pygame.draw.circle(surface=screen, color=(0, 255, 0), center=(x, y), radius=50)
-    pygame.draw.circle(
-        surface=screen,
-        color=(0, 255, 255),
-        center=ball[random.randint(1, 9)],
-        radius=50,
-    )
+    # pygame.draw.circle(
+    #     surface=screen,
+    #     color=(0, 255, 255),
+    #     center=ball[random.randint(1, 9)],
+    #     radius=50,
+    # )
+
+    screen.blit(image, ball_1[random.randint(1, 9)])
 
     # temp += 1
     # if temp > 5000:
